18f71b6ecd2e2875cc6ae793c261619c0c295359
youngbloodcyb <cameron.youngblood@gmail.com>
2023-02-05 16:34:51 -0700

    add nav component

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@
 module.exports = {
   content: [
     "./node_modules/flowbite-react/**/*.js",
-    "./pages/**/*.{ts,tsx}",
+    "./pages/**/*.{js,jsx}",
     "./public/**/*.html",
   ],
   plugins: [
