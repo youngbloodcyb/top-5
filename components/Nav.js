5b6802fda95e1aa6df8bc8f2c9bf4ec1af712ff5
youngbloodcyb <cameron.youngblood@gmail.com>
2023-02-05 17:19:42 -0700

    add logout functionality

--- components/Nav.js
+++ components/Nav.js
@@ -4,9 +4,9 @@ import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function Nav() {
     const { user } = useUser();
-    if (user) {
-    return (
-        <Navbar
+    if (!user) {
+        return (
+            <Navbar
         fluid={true}
         rounded={true}
         >
@@ -39,40 +39,40 @@ export default function Nav() {
             </Navbar.Link>
         </Navbar.Collapse>
         </Navbar>
-    )
+        )
     }
-    if (!user) {
-        return (
-            <Navbar
-            fluid={true}
-            rounded={true}
+    return (
+        user && (
+        <Navbar
+        fluid={true}
+        rounded={true}
+        >
+        <Navbar.Brand>
+            <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="mr-3 h-6 sm:h-9"
+            alt="Flowbite Logo"
+            />
+            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            Flowbite
+            </span>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+            <Navbar.Link
+            href="#"
+            active={true}
             >
-            <Navbar.Brand>
-                <img
-                src="https://flowbite.com/docs/images/logo.svg"
-                className="mr-3 h-6 sm:h-9"
-                alt="Flowbite Logo"
-                />
-                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-                Flowbite
-                </span>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Collapse>
-                <Navbar.Link
-                href="#"
-                active={true}
-                >
-                Home
-                </Navbar.Link>
-                <Navbar.Link href="#">
-                Services
-                </Navbar.Link>
-                <Navbar.Link href="/api/auth/logout">
-                Logout
-                </Navbar.Link>
-            </Navbar.Collapse>
-            </Navbar>
-        ) 
-    }
+            Home
+            </Navbar.Link>
+            <Navbar.Link href="#">
+            Services
+            </Navbar.Link>
+            <Navbar.Link href="/api/auth/logout">
+            Logout
+            </Navbar.Link>
+        </Navbar.Collapse>
+        </Navbar>
+    )
+    )
 }
