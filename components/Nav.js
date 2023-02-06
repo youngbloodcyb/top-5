0a3097c6fa4c740edfea571596e3ba951ced3e9e
youngbloodcyb <cameron.youngblood@gmail.com>
2023-02-05 17:13:13 -0700

    add Auth0

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,10 @@
 import { Navbar } from "flowbite-react"
 import React from "react"
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function Nav() {
+    const { user } = useUser();
+    if (user) {
     return (
         <Navbar
         fluid={true}
@@ -28,13 +31,48 @@ export default function Nav() {
             <Navbar.Link href="#">
             Services
             </Navbar.Link>
-            <Navbar.Link href="#">
+            <Navbar.Link href="/api/auth/login">
             Login
             </Navbar.Link>
-            <Navbar.Link href="#">
+            <Navbar.Link href="/api/auth/login">
             Sign-up
             </Navbar.Link>
         </Navbar.Collapse>
         </Navbar>
     )
+    }
+    if (!user) {
+        return (
+            <Navbar
+            fluid={true}
+            rounded={true}
+            >
+            <Navbar.Brand>
+                <img
+                src="https://flowbite.com/docs/images/logo.svg"
+                className="mr-3 h-6 sm:h-9"
+                alt="Flowbite Logo"
+                />
+                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+                Flowbite
+                </span>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse>
+                <Navbar.Link
+                href="#"
+                active={true}
+                >
+                Home
+                </Navbar.Link>
+                <Navbar.Link href="#">
+                Services
+                </Navbar.Link>
+                <Navbar.Link href="/api/auth/logout">
+                Logout
+                </Navbar.Link>
+            </Navbar.Collapse>
+            </Navbar>
+        ) 
+    }
 }
